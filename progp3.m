bd596db416d8eda5b2d24a0c4707ff7546f22807
Phil <potatopommes@gmail.com>
2018-06-10 16:03:53 +0200

    plotted figures, horner function now accepts vektor of xs that are going to be evaluated

--- progp3.m
+++ progp3.m
@@ -1,16 +1,35 @@
 %Aufgabe aus letztem ?ungsblatt:
-x=[-1,1,2]
-f=[2,6,4] 
+x3=[-1,1,2];
+f3=[2,6,4];
 %L?sung: a = 2, 2, -4/3
 
 %Beispielwerte aus dem Internet
-x2=[1,2,3,4]
-f2=[2,3,1,3]
+x4=[1,2,3,4];
+f4=[2,3,1,3];
 %L?sung: a = 2, 1, -1.5, 7/6
 
-disp(Horner2(x2,DivDiff(x2,f2)));
-DivDiff(x, f);
+plotCurves();
 
+function plotCurves()
+	x=-5:1/100:5;
+	j=1;
+	for n=4:2:10
+		x1 = [];
+		f1 = [];
+
+		for k=0:n
+			%xNeu = 10*k/n-5; %1. Datensatz
+			xNeu = 5*cos((2*k+1)/(2*k+2)*pi); %2. Datensatz
+			x1 = [x1, xNeu];
+			f1 = [f1, 1/(xNeu^2+1)];
+		end
+		a1 = DivDiff(x1, f1);
+		x1
+		f(j,:) = Horner2(x1, a1, x)
+		j=j+1;
+	end
+		plot(x, f(1,:), x, f(2,:), x, f(3,:), x, f(4,:));
+end
 
 
 
@@ -52,12 +71,12 @@ function y = Horner(x,a)
     y = temp;
 end
 
-function y = Horner2(x,a)
+function y = Horner2(x,a,xsToEval)
 
-	anzahlDerAs = size(a,2);
+	anzahlDerXs = size(xsToEval,2);
 
-	for i=1:anzahlDerAs
-		y(i) = HornerRecursive(x,a,1,x(i));
+	for i=1:anzahlDerXs
+		y(i) = HornerRecursive(x,a,1,xsToEval(i));
 	end
 end
 
@@ -65,7 +84,7 @@ function y = HornerRecursive(x,a,recStep,xToEval)
 	if (recStep > size(a,2))
 		y = 1;
 	else
-		y = a(recStep) + (xToEval - x(recStep)) * HornerRecursive(x,a,recStep+1, xToEval);
+		y = a(recStep) + (xToEval - x(recStep)) * (HornerRecursive(x,a,recStep+1, xToEval));
 	end
 end
 
